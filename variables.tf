77ffefb0331156d70a10b743cb33c7df07704aa1
mdaslamansari <mohd.aslam1@gmail.com>
2020-05-06 01:03:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,24 @@
-#variable "identity" {
- # description = "A unique name for your resources"
-#}
+variable "identity" {
+  description = "A unique name for your resources"
+}
 
-#variable "ami" {
- # description = "The Amazon Machine Image for new instances."
- # default     = "ami-c62eaabe"
-#}
+variable "ami" {
+  description = "The Amazon Machine Image for new instances."
+  default     = "ami-c62eaabe"
+}
 
 variable "ingress_cidr" {
   default     = "0.0.0.0/0"
   description = "IP block from which connections to this instance will be made"
 }
 
-#variable "public_key" {
-#  description = "Contents of the public key used to connect to this instance"
-#}
+variable "public_key" {
+  description = "Contents of the public key used to connect to this instance"
+}
 
-#variable "private_key" {
- # description = "Contents of the private key used to connect to this instance"
-#}
+variable "private_key" {
+  description = "Contents of the private key used to connect to this instance"
+}
 
 variable "num_webs" {
   description = "The number of servers to run"
