bab67edcf49ddf7f0e5eb677f7d04976a3cd8fa6
mdaslamansari <mohd.aslam1@gmail.com>
2020-05-06 00:49:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ module "server" {
   source = "./server"
 
   num_webs     = var.num_webs
-  #identity     = var.identity
- # ami          = var.ami
+  identity     = var.identity
+  ami          = var.ami
   ingress_cidr = var.ingress_cidr
-  #public_key   = var.public_key
-  #private_key  = var.private_key
+  public_key   = var.public_key
+  private_key  = var.private_key
 }
 
